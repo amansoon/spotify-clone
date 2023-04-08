35bd4bd6baf9c0eb8c68303dc599649db8211641
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-04-08 12:21:01 +0530

    create top track list

---
+++ src/icons/TrackPauseIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {};
+
+function TrackPauseIcon({}: Props) {
+  return (
+    <div>
+      <svg
+        height="16"
+        width="16"
+        aria-hidden="true"
+        viewBox="0 0 16 16"
+        data-encore-id="icon"
+        fill="white"
+      >
+        <path d="m4.7 2.404 8.993 5.192a0.467 0.467 0 0 1 0 0.808L4.7 13.596A0.467 0.467 0 0 1 4 13.192V2.808a0.467 0.467 0 0 1 0.7 -0.404z" />
+      </svg>
+    </div>
+  );
+}
+
+export default TrackPauseIcon;
---
+++ src/icons/TrackPlayIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+function TrackPlayIcon({}: Props) {
+  return (
+    <div>
+      <svg role="img" height="14" width="14" aria-hidden="true" viewBox="0 0 14 14" data-encore-id="icon" fill="white">
+        <path d="M2.363 0.875a0.612 0.612 0 0 0 -0.612 0.612v11.025a0.612 0.612 0 0 0 0.612 0.612h2.275a0.612 0.612 0 0 0 0.612 -0.612V1.488a0.612 0.612 0 0 0 -0.612 -0.612H2.363zm7 0a0.612 0.612 0 0 0 -0.612 0.612v11.025a0.612 0.612 0 0 0 0.612 0.612h2.275a0.612 0.612 0 0 0 0.612 -0.612V1.488a0.612 0.612 0 0 0 -0.612 -0.612h-2.275z" />
+      </svg>
+    </div>
+  );
+}
+
+export default TrackPlayIcon;
