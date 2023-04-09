d0078d300ff7f4ebddda81cca46e71e954be5251
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-04-09 22:18:15 +0530

    edit artist page

---
+++ src/icons/ArtistMoreOptionIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {};
+
+function ArtistMoreOptionIcon({}: Props) {
+  return (
+    <div>
+      <svg
+        role="img"
+        height="32"
+        width="32"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        data-encore-id="icon"
+        fill="gray"
+      >
+        <path d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
+      </svg>
+    </div>
+  );
+}
+
+export default ArtistMoreOptionIcon;
